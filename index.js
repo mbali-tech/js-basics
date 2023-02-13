08fa8ae57d0f287d0069974dfcbedc1ed4d24edf
Princess Nkabinde <nkabindembali3@gmail.com>
2023-02-13 16:03:58 +0200

    js basics commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+let num1 = 8;
+let num2 = 3;
+
+console.log(multiplyNumbers(num1, num2));
+
+
+let firstName = "Mbali";
+let lastName = "Nkabinde;"
+let stuternTrack = "Backend";
+let skills = "html,css,postgresql";
+let favouriteColor = "Grey";
+let indepedenceYear = "1994";
+
+let sentence = "My name is ${firstName} ${lastName} I am on the ${stuternTrack} track, my skills are ${skills}, my favourite color is ${favouriteColor} and my country gained its independence in ${indepedenceYear}";
+
+console.log(sentence);
